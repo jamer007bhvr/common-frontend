3e1069604919f27dee9464c32413a3eda2b838e0
Théo Mathieu <theo.mathieu1@gmail.com>
2017-01-16 11:42:57 -0500

    SendResetPassword = formBuilder

--- src/user/reset-password/send-reset-password.component.ts
+++ src/user/reset-password/send-reset-password.component.ts
@@ -1,29 +1,36 @@
 import { Component } from '@angular/core';
 import { ToastController } from 'ionic-angular';
 import { TranslateService } from 'ng2-translate';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { Api } from '../../api/api';
+import { GlobalValidator } from '../../form/global-validator';
 
 @Component({
 	selector: 'send-reset-password',
 	templateUrl: 'send-reset-password.html',
 })
 export class SendResetPasswordComponent {
-	credentials: { email: string } = {
-		email: 'theo.mathieu1@gmail.com',
-	};
+
+	email: FormControl = new FormControl('', [Validators.required, GlobalValidator.isEmail]);
+
+	submitted: Boolean = false;
+
+	form: FormGroup = this.formBuilder.group({
+		email: this.email
+	});
 
 	constructor(
+		private formBuilder: FormBuilder,
 		public api: Api,
 		public toastCtrl: ToastController,
-		public translateService: TranslateService) {
-	
-	}
+		public translateService: TranslateService) {}
 
 	// Attempt to login in through our User service
 	sendResetPassword() {
-
-		this.api.post('/send-reset-password', { email: this.credentials.email })
+		const sub = this.api.post('/send-reset-password', this.form.value);
+		
+		sub.map(res => res.json())
 			.subscribe(resp => {
 				this.translateService.get('SEND_RESET_PASSWORD_SUCCESS').subscribe(message => {
 					this.toastCtrl.create({
@@ -44,5 +51,15 @@ export class SendResetPasswordComponent {
 				});
 			});
 
+		return sub;
+
+	}
+
+
+	submit() {
+		this.submitted = true;
+		if (!this.form.valid) return;
+
+		return this.sendResetPassword();
 	}
 }
