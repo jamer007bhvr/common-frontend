2b03b75b5b341bbd0f4bbc91da613652127522d1
Théo Mathieu <theo.mathieu1@gmail.com>
2017-01-16 09:58:18 -0500

    feat: formBuilder used for login

--- src/user/login/login.component.ts
+++ src/user/login/login.component.ts
@@ -1,34 +1,40 @@
 import { Component } from '@angular/core';
 import { ToastController } from 'ionic-angular';
 import { TranslateService } from 'ng2-translate';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { UserService } from '../user.service';
 import { Api } from '../../api/api';
+import { GlobalValidator } from '../../form/global-validator';
 
 @Component({
 	selector: 'login-component',
 	templateUrl: 'login.html',
 })
 export class LoginComponent {
-	credentials: { email: string, password: string } = {
-		email: 'admin@gmail.com',
-		password: 'aaaa',
-	};
+
+	email: FormControl = new FormControl('', [Validators.required, GlobalValidator.isEmail]);
+	password: FormControl = new FormControl('', [Validators.required, Validators.minLength(4)]);
+
+	submitted: Boolean = false;
+
+	form: FormGroup = this.formBuilder.group({
+		email: this.email,
+		password: this.password
+	});
 
 	constructor(
+		private formBuilder: FormBuilder,
 		public api: Api,
-		public userService: UserService,
 		public toastCtrl: ToastController,
-		public translateService: TranslateService) {
-	
-	}
+		public translateService: TranslateService,
+		public userService: UserService) {}
 
 	// Attempt to login in through our User service
 	login() {
-		const seq = this.api.post('/login', this.credentials);
+		const sub = this.api.post('/login', this.form.value);
 		
-		seq
-			.map(res => res.json())
+		sub.map(res => res.json())
 			.subscribe(res => {
 				this.userService.loggedIn(res);
 				// redirect ?
@@ -44,7 +50,15 @@ export class LoginComponent {
 				});
 			});
 
-		return seq;
+		return sub;
 
+	}
+
+
+	submit() {
+		this.submitted = true;
+		if (!this.form.valid) return;
+
+		return this.login();
 	}
 }
