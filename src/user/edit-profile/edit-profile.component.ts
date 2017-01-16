a0b43831e20ff96356da0405efed7ea8b6a8ae2d
Théo Mathieu <theo.mathieu1@gmail.com>
2017-01-16 11:29:50 -0500

    feat: edit profile

---
+++ src/user/edit-profile/edit-profile.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { ToastController } from 'ionic-angular';
+import { TranslateService } from 'ng2-translate';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { UserService } from '../user.service';
+import { Api } from '../../api/api';
+import { GlobalValidator } from '../../form/global-validator';
+
+@Component({
+	selector: 'edit-profile',
+	templateUrl: 'edit-profile.html',
+})
+export class EditProfileComponent {
+
+	firstName: FormControl = new FormControl('', [Validators.required]);
+	lastName: FormControl = new FormControl('', [Validators.required]);
+
+	submitted: Boolean = false;
+
+	form: FormGroup = this.formBuilder.group({
+		firstName: this.firstName,
+		lastName: this.lastName
+	});
+
+	constructor(
+		private formBuilder: FormBuilder,
+		public api: Api,
+		public toastCtrl: ToastController,
+		public translateService: TranslateService,
+		public userService: UserService) {}
+
+	// Attempt to login in through our User service
+	editProfile() {
+		const sub = this.api.put('/user', this.form.value);
+		
+		sub.map(res => res.json())
+			.subscribe(res => {
+				this.translateService.get('EDIT_PROFILE_SUCCESS').subscribe(message => {
+					this.toastCtrl.create({
+						message: message,
+						duration: 3000,
+						position: 'top',
+					}).present();
+				});
+				// redirect ?
+				// this.navCtrl.push(MainPage);
+
+			}, err => {
+				this.translateService.get('EDIT_PROFILE_ERROR').subscribe(message => {
+					this.toastCtrl.create({
+						message: message,
+						duration: 3000,
+						position: 'top',
+					}).present();
+				});
+			});
+
+		return sub;
+
+	}
+
+
+	submit() {
+		this.submitted = true;
+		if (!this.form.valid) return;
+
+		return this.editProfile();
+	}
+}
